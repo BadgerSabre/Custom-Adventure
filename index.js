7508b5d257fa89df8369cefd9447963df73d3deb
Brandon Dega <brandondega@gmail.com>
2022-01-31 13:45:29 -0600

    fixing first question prompt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let firstAnswer = window.prompt('Do you levetate or sneak?')
 switch(firstAnswer) {
     case 'levetate':
         window.prompt('You quickly float to the top of the walls.  There is no one here.  There are two seperate towers. Do you explore the right or left?')
