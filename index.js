05d46fec5639a15518262dc27129bd79c5d618e6
Brandon Dega <brandondega@gmail.com>
2022-01-31 13:36:48 -0600

    adding more cases

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 switch(firstAnswer) {
     case 'levetate':
         window.prompt('You quickly float to the top of the walls.  There is no one here.  There are two seperate towers. Do you explore the right or left?')
+    break
+    case 'sneak':
+        window.prompt('You enter the crypt of the castle.  There is a horde of ghouls that notice you and charge you. Do you attempt to outrun them or cast control undead?')
 }
