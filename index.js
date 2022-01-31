2d7df81afaae4097f965965a0f12569769c603cc
Brandon Dega <brandondega@gmail.com>
2022-01-31 12:56:12 -0600

    js folder

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+switch(firstAnswer) {
+    case 'levetate':
+        window.prompt('You quickly float to the top of the walls.  There is no one here.  There are two seperate towers. Do you explore the right or left?')
+}
