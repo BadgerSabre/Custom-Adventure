d5963cbba509e67dfb2c6b699bdea27f5f0d6ea9
Brandon Dega <brandondega@gmail.com>
2022-02-01 12:58:40 -0600

    please work

--- index.js
+++ index.js
@@ -25,11 +25,35 @@ switch(firstAnswer) {
                             case 'staircase':
                                 window.alert('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
                             break
-
                         }
+                break
             }
     break
     case 'right':
-        window.prompt('You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?')
+       let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?')
+            switch(secondAnswer){
+                case 'past the dragon':
+                    let thirdAnswer = window.prompt('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run.')
+                        switch(thirdAnswer){
+                            case 'stay':
+                                window.alert('You and the dragon have an uplifting conversation about local politics and become lifelong friends.')
+                            break
+                            case 'run':
+                                window.alert(`Quickly, you run back to the cave's entrance. Sheepish, you return home.`)
+                            break
+                        }
+                break
+                case 'away from dragon':
+                    let thirdAnswer = window.prompt('After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?')
+                    switch(thirdAnswer){
+                        case 'draw it':
+                            window.alert('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.')
+                        break
+                        case 'pick it':
+                            window.alert('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
+                        break
+                    }
+                break
+            }
     break
 }
