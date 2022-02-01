60784a25fd845c699a7ade3096b59569db31d3c0
Brandon Dega <brandondega@gmail.com>
2022-02-01 10:07:18 -0600

    more answers added

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ switch(firstAnswer) {
     case 'levetate':
         window.prompt('You quickly float to the top of the walls.  There is no one here.  There are two seperate towers. Do you explore the right or left?')
             switch(secondAnswer) {
-                case 'right' :
+                case 'right':
                     window.prompt('You descend stairs into the throne room.  There is a red dragon glaring at you.  You see the stone under his front claw.  Do you cast fireball or ray of frost?')
+                        switch(finalAnswer){
+                            case 'fireball' :
+                                window.alert('You really casted a fireball at a red dragon?  It has no affect, the dragon breathes fire and incinerates you.  Refresh to try again.')
+                            break
+                            case 'ray of frost' :
+                                window.alert('Citrical hit! The spell pierces the dragons scales and hit it right in the heart.  The dragon collapses dead and the stone is yours!')
+                        }
                 break
-                case 'left' :
+                case 'left':
                     window.prompt('You descend stairs into the dungeon.  There is a suspicious man in a cell that calls out to you, "I know why you are here and I can help you obtain the stone. If only you would set me free." Do you set him free or let him rot?')
+                break
             }
     break
     case 'sneak':
         window.prompt('You enter the crypt of the castle.  There is a horde of ghouls that notice you and charge you. Do you attempt to outrun them or cast control undead?')
             switch(secondAnswer) {
-                case 'attempt to outrun' :
-                    window.prompt('Are you crazy? The ghouls easily catch you and rip you to pieces.  Refresh to try again.')
+                case 'attempt to outrun':
+                    window.alert('Are you crazy? The ghouls easily catch you and rip you to pieces.  Refresh to try again.')
                 break
-                case 'cast control undead' :
+                case 'cast control undead':
                     window.prompt('The ghouls stop a few feet before you.  One points to a door at the end of a hallway. Do you follow the ghoul through the door or do you go back the way you came?')
+                break
             }
+    break
 }
