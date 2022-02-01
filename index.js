c139aef83dd755888037a04c1c8d975c607b0520
Brandon Dega <brandondega@gmail.com>
2022-02-01 10:29:58 -0600

    finished custom switch adventure

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ switch(firstAnswer) {
                 case 'right':
                     window.prompt('You descend stairs into the throne room.  There is a red dragon glaring at you.  You see the stone under his front claw.  Do you cast fireball or ray of frost?')
                         switch(finalAnswer){
-                            case 'fireball' :
+                            case 'fireball':
                                 window.alert('You really casted a fireball at a red dragon?  It has no affect, the dragon breathes fire and incinerates you.  Refresh to try again.')
                             break
-                            case 'ray of frost' :
+                            case 'ray of frost':
                                 window.alert('Citrical hit! The spell pierces the dragons scales and hit it right in the heart.  The dragon collapses dead and the stone is yours!')
+                            break
                         }
                 break
                 case 'left':
                     window.prompt('You descend stairs into the dungeon.  There is a suspicious man in a cell that calls out to you, "I know why you are here and I can help you obtain the stone. If only you would set me free." Do you set him free or let him rot?')
+                    switch(finalAnswer){
+                        case 'set him free':
+                            window.alert('You open the cell door and he presses a hidden button on the wall behind you.The wall opens revealing a room filled with treasure, including the sacred stone!')
+                        break
+                        case 'let him rot':
+                            window.alert('You wander the dungeon for an hour with no luck.  Your torch runs out and you are lost in the darkness. Refresh to try again.')
+                            break
+                    }
                 break
             }
     break
@@ -25,7 +34,15 @@ switch(firstAnswer) {
                     window.alert('Are you crazy? The ghouls easily catch you and rip you to pieces.  Refresh to try again.')
                 break
                 case 'cast control undead':
-                    window.prompt('The ghouls stop a few feet before you.  One points to a door at the end of a hallway. Do you follow the ghoul through the door or do you go back the way you came?')
+                    window.prompt('The ghouls stop a few feet before you.  One walks toward a door at the end of a hallway and motions for you to follow. Do you follow the ghoul through the door or do you go back the way you came?')
+                        switch(finalAnswer){
+                            case 'follow':
+                                window.alert('The ghoul leads you to an alchemist lab.  You look through the notes on the table and discover the recipe to turn iron into gold!')
+                            break
+                            case 'go back':
+                                window.alert('You are a coward, but at least you are alive. Refresh to try again.')
+                            break
+                        }
                 break
             }
     break
